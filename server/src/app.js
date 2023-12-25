59620d4a47d5dd197923fcdc20ae590b3f1c5179
ayush.mittal <ayush.mittal@maximl.com>
2023-12-25 22:01:09 +0530

    all setup configured

--- server/src/app.js
+++ server/src/app.js
@@ -13,4 +13,12 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"})) // here the basically the url search will happen like in the google 
 app.use(express.static("public")) // this is use for the extra files like imgaes, fonts, etc. 
 app.use(cookieParser()) // this is just to access the cookies for the better user experience 
+
+//routes import
+
+import userRouter from './routes/user.route.js'
+
+//routes declartion 
+app.use("/api/v1/users",userRouter);
+
 export { app }
