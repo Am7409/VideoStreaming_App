a8ca5475c842e2ae40be55a3edc7b33bc4060cfa
ayush.mittal <ayush.mittal@maximl.com>
2023-12-28 20:24:03 +0530

    Implement user authentication features.

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,6 +1,7 @@
 import {Router} from 'express';
-import { registerUser } from '../controllers/user.controller.js';
+import { loggOutUser, loginUser, refreshAcessToken, registerUser } from '../controllers/user.controller.js';
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from '../middlewares/auth.middleware.js';
 
 const router = Router()
 
@@ -16,6 +17,10 @@ router.route("/register").post(
     ]),
     registerUser
     )
+
+    router.route("/login").post(loginUser)
+    router.route("/logout").post(verifyJWT , loggOutUser)
+    router.route("/refresh-token").post(refreshAcessToken)
 
 
 export default router
