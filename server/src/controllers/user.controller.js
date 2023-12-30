ef3ae6cab5b027735600a16e1290bf5faf50f920
ayush.mittal <ayush.mittal@maximl.com>
2023-12-30 18:57:14 +0530

    correcting the error

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -240,7 +240,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully");
+    .json(new ApiResponse(200, req.user, "current user fetched successfully"));
 });
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -265,23 +265,32 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is missing");
   }
+
   const avatar = await uploadOnCloudinary(avatarLocalPath);
+
   if (!avatar) {
-    throw new ApiError(400, "Error while uploading on avatar");
-  }
+    throw new ApiError(400, "Error while uploading avatar");
+  } 
+
+  const oldAvatarUrl = await User.findById(req.user?._id).select("avatar"); 
 
   const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: { avatar: avatar.url },
     },
-    { new: true }.select("-password")
-  );
+    { new: true } 
+  ).select("-password"); 
+
+  if(oldAvatarUrl && oldAvatarUrl.avatar){
+    await deleteFromCloudinary(oldAvatarUrl.avatar);
+ }
 
   return res
     .status(200)
-    .json(new ApiResponse(200, user, "The avatar is upadated successfully"));
+    .json(new ApiResponse(200, user, "The avatar is updated successfully"));
 });
+
 
 const updateUserCoverImage = asyncHandler(async (req, res) => {
   const coverImageLocalPath = req.file?.path;
@@ -299,8 +308,8 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     {
       $set: { coverImage: coverImage.url },
     },
-    { new: true }.select("-password")
-  );
+    { new: true }
+  ).select("-password");
 
   return res
     .status(200)
